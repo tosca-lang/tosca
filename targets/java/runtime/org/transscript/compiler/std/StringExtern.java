bb48252420da75af16a3bd45374429ce391ff9cf
Lionel Villard <villard@us.ibm.com>
2016-04-29 16:05:28 -0400

    cleanup a bit the core string library

--- targets/java/runtime/org/transscript/compiler/std/StringExtern.java
+++ targets/java/runtime/org/transscript/compiler/std/StringExtern.java
@@ -4,10 +4,8 @@ package org.transscript.compiler.std;
 
 import static org.transscript.compiler.std.Core.FALSE;
 import static org.transscript.compiler.std.Core.TRUE;
-import static org.transscript.compiler.std.Core.lazyBool;
 import static org.transscript.runtime.DoubleTerm.doubleTerm;
 import static org.transscript.runtime.StringTerm.stringTerm;
-import static org.transscript.runtime.StringTerm.lazyStringTerm; 
 import static org.transscript.runtime.Term.force;
 
 import java.io.FileNotFoundException;
@@ -34,32 +32,38 @@ import org.transscript.runtime.utils.StringUtils;
 public class StringExtern
 {
 
-	public static StringTerm Rescape(Context context, StringTerm x_634)
+	/**
+	 * @param context
+	 * @param string
+	 * @param search
+	 * @return TRUE if and only if the first given string contains the second given string
+	 */
+	public static Bool Contains(Context context, StringTerm string, StringTerm search)
 	{
-		throw new RuntimeException();
-	}
+		string = force(context, string);
+		search = force(context, search);
 
-	public static StringTerm AfterFirst(Context context, StringTerm str, StringTerm first)
-	{
-		StringTerm result;
-		int index = str.unbox().indexOf(first.unbox());
-		if (index == -1)
-			result = stringTerm("");
-		else
-			result = stringTerm(str.unbox().substring(index + first.unbox().length()));
-
-		str.release();
-		first.release();
-		return result;
-	}
-
-	public static StringTerm Mangle(Context context, StringTerm x_661)
-	{
-		throw new RuntimeException();
+		final boolean result = string.unbox().contains(search.unbox());
+		string.release();
+		search.release();
+		return result ? TRUE(context) : FALSE(context);
 	}
 
 	/**
-	 * 
+	 * @param context
+	 * @param str
+	 * @return the length of the given string *
+	 */
+	public static DoubleTerm Length(Context context, StringTerm str)
+	{
+		str = force(context, str);
+		DoubleTerm result = doubleTerm(str.unbox().length());
+		str.release();
+		return result;
+	}
+
+	/**
+	 * Tests if the beginning of the given string starts with the specified prefix.
 	 * @param context
 	 * @param str
 	 * @param prefix
@@ -69,15 +73,80 @@ public class StringExtern
 	{
 		StringTerm estr = force(context, str);
 		StringTerm eprefix = force(context, prefix);
-		if (estr.data() && eprefix.data())
-		{
-			boolean result = estr.unbox().startsWith(eprefix.unbox());
-			estr.release();
-			eprefix.release();
-			return result ? Core.TRUE(context) : Core.FALSE(context);
-		}
 
-		return Core.lazyBool(c -> StartsWith(context, estr, eprefix));
+		final Bool result = estr.unbox().startsWith(eprefix.unbox()) ? TRUE(context) : FALSE(context);
+		estr.release();
+		eprefix.release();
+		return result;
+	}
+
+	/**
+	 * Tests if the ending of the given string finishes with the specified suffix.
+	 * @param context
+	 * @param string
+	 * @param suffix
+	 * @return
+	 */
+	public static Bool EndsWith(Context context, StringTerm str, StringTerm suffix)
+	{
+		StringTerm estr = force(context, str);
+		StringTerm esuffix = force(context, suffix);
+
+		final Bool result = estr.unbox().endsWith(esuffix.unbox()) ? TRUE(context) : FALSE(context);
+
+		estr.release();
+		esuffix.release();
+		return result;
+	}
+
+	/**
+	 * @param context
+	 * @param str1
+	 * @param str2
+	 * @return TRUE if and only if the given strings are equals.
+	 */
+	public static Bool StringEqual(Context context, StringTerm str1, StringTerm str2)
+	{
+		final StringTerm estr1 = force(context, str1);
+		final StringTerm estr2 = force(context, str2);
+		final Bool result = estr1.unbox().equals(estr2.unbox()) ? TRUE(context) : FALSE(context);
+		estr1.release();
+		estr2.release();
+		return result;
+	}
+
+	/**
+	 * @param context
+	 * @param str
+	 * @param first
+	 * @return the string after the first occurrence of the given {@code first} string, or 
+	 *         the empty string if there is no such occurrences
+	 */
+	public static StringTerm AfterFirst(Context context, StringTerm str, StringTerm first)
+	{
+		final StringTerm estr = force(context, str);
+		final StringTerm efirst = force(context, first);
+
+		StringTerm result;
+		int index = estr.unbox().indexOf(efirst.unbox());
+		if (index == -1)
+			result = stringTerm("");
+		else
+			result = stringTerm(estr.unbox().substring(index + efirst.unbox().length()));
+
+		estr.release();
+		efirst.release();
+		return result;
+	}
+
+	public static StringTerm Rescape(Context context, StringTerm x_634)
+	{
+		throw new RuntimeException();
+	}
+
+	public static StringTerm Mangle(Context context, StringTerm x_661)
+	{
+		throw new RuntimeException();
 	}
 
 	public static <a extends Term> a ParseText(Context context, ThunkMaker<a> tma, StringTerm x_695, StringTerm x_700)
@@ -173,28 +242,24 @@ public class StringExtern
 		StringTerm estr = Term.force(context, str);
 		StringTerm eold = Term.force(context, old);
 		StringTerm enew = Term.force(context, news);
-		if (estr.data() && eold.data() && enew.data())
-		{
-			StringTerm result = stringTerm(estr.unbox().replace(eold.unbox(), enew.unbox()));
-			estr.release();
-			eold.release();
-			enew.release();
-			return result;
-		}
-
-		return StringTerm.lazyStringTerm(c -> Replace(context, estr, eold, enew));
+		StringTerm result = stringTerm(estr.unbox().replace(eold.unbox(), enew.unbox()));
+		estr.release();
+		eold.release();
+		enew.release();
+		return result;
 	}
 
 	/**
-	 * 
+	 * Trim leading and trailing spaces
 	 * @param context
 	 * @param str
 	 * @return
 	 */
 	public static StringTerm Trim(Context context, StringTerm str)
 	{
-		StringTerm result = stringTerm(str.unbox().trim());
-		str.release();
+		StringTerm estr = Term.force(context, str);
+		StringTerm result = stringTerm(estr.unbox().trim());
+		estr.release();
 		return result;
 	}
 
@@ -209,21 +274,10 @@ public class StringExtern
 	{
 		final StringTerm estr1 = force(context, str1);
 		final StringTerm estr2 = force(context, str2);
-		if (estr1.data() && estr2.data())
-		{
-			StringTerm result = stringTerm(estr1.unbox() + estr2.unbox());
-			estr1.release();
-			estr2.release();
-			return result;
-		}
-		return lazyStringTerm(c -> ConcatString(c, estr1, estr2));
-	}
 
-	public static DoubleTerm Length(Context context, StringTerm str)
-	{
-		DoubleTerm result = doubleTerm(str.unbox().length());
-
-		str.release();
+		StringTerm result = stringTerm(estr1.unbox() + estr2.unbox());
+		estr1.release();
+		estr2.release();
 		return result;
 	}
 
@@ -235,77 +289,14 @@ public class StringExtern
 	 */
 	public static StringTerm FormatNumber(Context context, DoubleTerm num)
 	{
-		DoubleTerm evalnum = num.eval(context);
-		if (evalnum.data())
-		{
+		DoubleTerm evnum = force(context, num);
 
-			// FIXME
-			StringTerm result = stringTerm(Integer.toString((int) evalnum.unbox()));
-			evalnum.release();
+		// FIXME
+		StringTerm result = stringTerm(Integer.toString((int) evnum.unbox()));
+		evnum.release();
 
-			return result;
-
-		}
-		return StringTerm.lazyStringTerm(c -> FormatNumber(c, evalnum));
+		return result;
 	}
-
-	/**
-	 * 
-	 * @param context
-	 * @param str1
-	 * @param str2
-	 * @return
-	 */
-	public static Bool StringEqual(Context context, StringTerm str1, StringTerm str2)
-	{
-		final StringTerm estr1 = force(context, str1);
-		final StringTerm estr2 = force(context, str2);
-		if (estr1.data() && estr2.data())
-		{
-			final boolean result = estr1.unbox().equals(estr2.unbox());
-			estr1.release();
-			estr2.release();
-			return result ? TRUE(context) : FALSE(context);
-		}
-		return lazyBool(c -> StringEqual(c, estr1, estr2));
-		
-	}
-
-	public static Bool Contains(Context context, StringTerm string, StringTerm search)
-	{
-		string.eval(context).release();
-		search.eval(context).release();
-
-		if (string.data() && search.data())
-		{
-			final boolean result = string.unbox().contains(search.unbox());
-			string.release();
-			search.release();
-			return result ? TRUE(context) : FALSE(context);
-		}
-
-		return Core.lazyBool(c -> Contains(c, string, search));
-	}
-
-	/**
-	 * 
-	 * @param context
-	 * @param string
-	 * @param suffix
-	 * @return
-	 */
-	public static Bool EndsWith(Context context, StringTerm string, StringTerm suffix)
-	{
-		if (!string.data() || !suffix.data())
-			throw new RuntimeException("Invalid argument in EndsWith");
-
-		final Bool b = string.unbox().endsWith(suffix.unbox()) ? TRUE(context) : FALSE(context);
-
-		string.release();
-		suffix.release();
-		return b;
-	}
-
 
 	/**
 	 * 
